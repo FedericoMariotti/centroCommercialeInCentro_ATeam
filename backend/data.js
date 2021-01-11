5b5061959672ce4d5f1af572e22155ab92b7e9eb
bemar <federico.mariotti@studenti.unicam.it>
2021-01-11 22:37:59 +0100

    prima iterazione

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+export default {
+    products: [
+        {
+        _id: '1',
+        name: 'Maglietta nera palma',
+        category: 'clothing',
+        image: './images/a1.jpg',
+        price: 50,
+        brand: 'Mike',
+        rating: 4.5,
+        numReiews: 10
+    },
+    {
+        _id: '2',
+        name: 'Maglietta nera rondine',
+        image: './images/a2.jpg',
+        price: 40,
+        brand: 'Abibas',
+        rating: 4.0,
+        numReiews: 13
+    },
+    {
+        _id: '3',
+        name: 'Canotta nera rondine',
+        image: './images/a3.jpg',
+        price: 35,
+        brand: 'Lacaste',
+        rating: 4.5,
+        numReiews: 8
+    },
+    {
+        _id: '4',
+        name: 'Maglietta nera semplice',
+        image: './images/a4.jpg',
+        price: 20,
+        brand: 'Guddi',
+        rating: 5,
+        numReiews: 22
+    },
+
+]
+}
